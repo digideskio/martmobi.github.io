6d1551a84927a97bc44d73023eb7fb422c801ce8
Akash Bhardwaj <bhardwaj.akash@snapdeal.com>
2015-05-14 18:06:56 +0530

    Buyer page added

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ $(document).on('click','#login-submit', function(e) {
         alert ('Please do not leave the field empty');
     }
     else{
+    $('.form-area').css('display', 'none');
+    $('.msg').css('display', 'block');
     $('.treasure-bg').css('opacity', '0.2');
     $('.ajax-loader').css('display', 'block');
     var name = $('#name').val();
@@ -43,12 +45,59 @@ $(document).on('click','#login-submit', function(e) {
            });
     sendmail();
 
-    var sellercount = $('.seller-count').html();
+    var sellercount = $('.seller-count').text();
     sellercount = parseInt(sellercount) + 3;
     localStorage.setItem('sellerCount', sellercount);
     $('.seller-count').html(localStorage.getItem('sellerCount'));
 
     
+  }
+});
+
+//for buyer
+
+$(document).on('click','#login-submit-buyer', function(e) {
+  e.preventDefault();
+    if ($('#name').val() == "" || $('#phonenumber').val() == "" || $('#email').val() == "" || $('#city').val() == "None" ){
+        alert ('Please do not leave the field empty');
+    }
+    else{
+    $('.form-area').css('display', 'none');
+    $('.msg').css('display', 'block');
+    $('.treasure-bg').css('opacity', '0.2');
+    $('.ajax-loader').css('display', 'block');
+    var name = $('#name').val();
+    var phonenumber = $('#phonenumber').val();
+    var city = $('#city').val();
+    var email = $('#email').val();
+    $.ajax({
+
+
+                url: "https://docs.google.com/a/snapdeal.com/forms/d/1nBNWIPTNQawmw49xvyuUXE9tWRb7wePaNjpPT8jjYak/formResponse",
+                data: {'entry_1541905258' : name, 'entry_195017034' : email,  'entry_1374944601' : phonenumber, 'entry_329537662' : city},
+                type: "POST",
+                dataType: "xml",
+                statusCode: {
+                    0: function (){
+ 
+                        $('#name').val();
+                        $('#email').val();
+                        $('#phonenumber').val();
+                        $('#city').val();
+                        //Success message
+                    },
+                    200: function (){
+                        $('#name').val();
+                        $('#email').val();
+                        $('#phonenumber').val();
+                        $('#city').val();
+                        
+                    }
+                }
+
+         
+           });
+    sendmailbuyer();
   }
 });
 
@@ -62,13 +111,39 @@ function sendmail(){
                 'from_email': 'admin@getshopo.com',
                 'to': [
                     {
-                      'email': 'hasith.goli@snapdeal.com',
+                      'email': 'sellersupport@getshopo.com',
                       'type': 'to'
                     }
                   ],
                 'autotext': 'true',
-                'subject': 'Treasure Signup',
-                'html': 'Hey Team! We have a signup for Treasure App <br> <br> Name - ' + $('#name').val() + '<br>Phone Number - ' + $('#phonenumber').val() + '<br>Location - ' + $('#location').val() + '<br>Category - ' + $('#category').val()
+                'subject': 'Shopo Signup',
+                'html': 'Hey Team! We have a signup for Shopo App <br> <br> Name - ' + $('#name').val() + '<br>Phone Number - ' + $('#phonenumber').val() + '<br>Location - ' + $('#location').val() + '<br>Category - ' + $('#category').val()
+              }
+          }
+          }).done(function(response) { 
+            $('.treasure-bg').css('opacity', '1');
+            $('.ajax-loader').css('display', 'none');
+             alert("Thankyou for Signing Up.");
+           });
+ }
+
+ function sendmailbuyer(){
+    $.ajax({
+            type: "POST",
+            url: "https://mandrillapp.com/api/1.0/messages/send.json",
+            data: {
+              'key': '1sRf8q5z9P969SAP-N_MpQ',
+              'message': {
+                'from_email': 'admin@getshopo.com',
+                'to': [
+                    {
+                      'email': 'support@getshopo.com',
+                      'type': 'to'
+                    }
+                  ],
+                'autotext': 'true',
+                'subject': 'Shopo Signup',
+                'html': 'Hey Team! We have a Buyer signup for Shopo App <br> <br> Name - ' + $('#name').val() + '<br>Email - ' + $('#email').val() + '<br>Phone Number - ' + $('#phonenumber').val() + '<br>City - ' + $('#city').val()
               }
           }
           }).done(function(response) { 
